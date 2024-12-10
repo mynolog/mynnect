e023895d6999f1a24044c6d4d70b630e72f7c033
mynolog <minholee.log@gmail.com>
2024-12-10 16:14:09 +0900

    💄 style: Navigation 활성화된 탭 강조하여 UX 개선

--- src/components/layout/Navigation/Navigation.tsx
+++ src/components/layout/Navigation/Navigation.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+import { useRouter, usePathname } from 'next/navigation'
 import { RiAccountCircle2Line } from 'react-icons/ri'
 import { FaSearch } from 'react-icons/fa'
 import AvatarImage from '@/components/common/Image/AvatarImage'
@@ -13,10 +13,17 @@ export default function Navigation() {
   const [isMounted, setIsMounted] = useState(false)
   const { user } = useUser()
   const router = useRouter()
+  const pathname = usePathname()
+
+  const isProfileActive = pathname === `/profile/${user?.uid}`
 
   useEffect(() => {
     setIsMounted(true)
-  }, [])
+  }, [pathname])
+
+  const handleNavigation = (href: string) => {
+    router.push(href)
+  }
 
   return (
     <nav className="fixed top-3 right-3 h-[97%] w-1/6 flex flex-col items-center gap-20 px-3 py-4 rounded-xl border-4 border-lime-green-300 hover:border-lime-green-900 transition-colors ease-in-out duration-300 font-bold text-md">
@@ -29,18 +36,27 @@ export default function Navigation() {
         <FaSearch className="text-lime-green-900" />
       </div>
       <ul className="w-full flex flex-col gap-3">
-        {navigationList.map((item) => (
-          <li
-            key={`${item.id}-${item.label}`}
-            className="flex px-3 w-full h-10 rounded-xl opacity-80 hover:bg-lime-green-300 hover:opacity-95 hover:cursor-pointer transition-all duration-200 ease-linear"
-          >
-            <Link href={item.href} className="flex items-center gap-3">
-              <span className="text-2xl">{item.icon}</span>
-              <span className="font-bold">{item.label}</span>
-            </Link>
-          </li>
-        ))}
-        <li className="flex px-3 w-full h-10 rounded-xl opacity-80 hover:bg-lime-green-300 hover:opacity-100 transition-all duration-200 ease-linear">
+        {navigationList.map((item) => {
+          const isActive = pathname === item.href
+
+          return (
+            <li
+              key={`${item.id}-${item.label}`}
+              onClick={() => handleNavigation(item.href)}
+              className={`${isActive ? 'bg-lime-green-300 opacity-100' : ''} flex px-3 w-full h-10 rounded-xl opacity-80 hover:bg-lime-green-300 hover:opacity-95 hover:cursor-pointer transition-all duration-200 ease-linear`}
+            >
+              <Link href={item.href} className="flex items-center gap-3">
+                <span className="text-2xl">{item.icon}</span>
+                <span className="font-bold">{item.label}</span>
+              </Link>
+            </li>
+          )
+        })}
+
+        <li
+          className={`${isProfileActive ? 'bg-lime-green-300 opacity-100' : ''} flex px-3 w-full h-10 rounded-xl opacity-80 hover:bg-lime-green-300 hover:opacity-100 hover:cursor-pointer transition-all duration-200 ease-linear`}
+          onClick={() => handleNavigation(`/profile/${user.uid}`)}
+        >
           <Link href={`/profile/${user?.uid}`} className="flex items-center gap-2">
             {user && isMounted ? (
               <AvatarImage
