4944125228c54f4adce13088eb9d39c4562d8061
mynolog <minholee.log@gmail.com>
2024-12-10 15:42:17 +0900

    💄 style: Navigation UI 변경

--- src/components/layout/Navigation/Navigation.tsx
+++ src/components/layout/Navigation/Navigation.tsx
@@ -2,27 +2,24 @@
 
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
-import {
-  RiHomeLine,
-  RiNotification4Line,
-  RiArrowRightUpLine,
-  RiEarthFill,
-  RiAccountCircle2Line,
-} from 'react-icons/ri'
+import { useRouter } from 'next/navigation'
+import { RiAccountCircle2Line } from 'react-icons/ri'
 import { FaSearch } from 'react-icons/fa'
 import AvatarImage from '@/components/common/Image/AvatarImage'
 import { useUser } from '@/hooks/useUser'
+import { navigationList } from '@/config/NavigationList'
 
 export default function Navigation() {
   const [isMounted, setIsMounted] = useState(false)
   const { user } = useUser()
+  const router = useRouter()
 
   useEffect(() => {
     setIsMounted(true)
   }, [])
 
   return (
-    <nav className="fixed top-3 right-3 h-[97%] w-1/6 flex flex-col items-center gap-20 px-6 py-4 rounded-xl border-4 border-lime-green-300 hover:border-lime-green-900 transition-colors ease-in-out duration-300 font-bold text-md">
+    <nav className="fixed top-3 right-3 h-[97%] w-1/6 flex flex-col items-center gap-20 px-3 py-4 rounded-xl border-4 border-lime-green-300 hover:border-lime-green-900 transition-colors ease-in-out duration-300 font-bold text-md">
       <div className="flex justify-center items-center gap-2">
         <input
           type="text"
@@ -31,31 +28,19 @@ export default function Navigation() {
         />
         <FaSearch className="text-lime-green-900" />
       </div>
-      <ul className="w-full flex flex-col gap-10">
-        <li className="w-full">
-          <Link href="/home" className="flex items-center gap-2">
-            <RiHomeLine className="text-2xl" />홈
-          </Link>
-        </li>
-        <li className="w-full">
-          <Link href="/explore" className="flex items-center gap-2">
-            <RiEarthFill className="text-2xl" />
-            탐색
-          </Link>
-        </li>
-        <li className="w-full">
-          <Link href="/trend" className="flex items-center gap-2">
-            <RiArrowRightUpLine className="text-2xl" />
-            트렌드
-          </Link>
-        </li>
-        <li className="w-full">
-          <Link href="/notification" className="flex items-center gap-2">
-            <RiNotification4Line className="text-2xl" />
-            알림
-          </Link>
-        </li>
-        <li className="w-full">
+      <ul className="w-full flex flex-col gap-3">
+        {navigationList.map((item) => (
+          <li
+            key={`${item.id}-${item.label}`}
+            className="flex px-3 w-full h-10 rounded-xl opacity-80 hover:bg-lime-green-300 hover:opacity-95 hover:cursor-pointer transition-all duration-200 ease-linear"
+          >
+            <Link href={item.href} className="flex items-center gap-3">
+              <span className="text-2xl">{item.icon}</span>
+              <span className="font-bold">{item.label}</span>
+            </Link>
+          </li>
+        ))}
+        <li className="flex px-3 w-full h-10 rounded-xl opacity-80 hover:bg-lime-green-300 hover:opacity-100 transition-all duration-200 ease-linear">
           <Link href={`/profile/${user?.uid}`} className="flex items-center gap-2">
             {user && isMounted ? (
               <AvatarImage
---
+++ src/config/NavigationList.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import {
+  RiHomeLine,
+  RiNotification4Line,
+  RiArrowRightUpLine,
+  RiEarthFill,
+  RiAccountCircle2Line,
+} from 'react-icons/ri'
+
+type NavigationItem = {
+  id: string
+  icon: React.ReactNode
+  label: string
+  href: string
+}
+
+export const navigationList: NavigationItem[] = [
+  {
+    id: '1',
+    icon: <RiHomeLine />,
+    label: '홈',
+    href: '/home',
+  },
+  {
+    id: '2',
+    icon: <RiEarthFill />,
+    label: '탐색',
+    href: '/explore',
+  },
+  {
+    id: '3',
+    icon: <RiArrowRightUpLine />,
+    label: '트렌드',
+    href: '/trend',
+  },
+  {
+    id: '4',
+    icon: <RiNotification4Line />,
+    label: '알림',
+    href: '/notification',
+  },
+]
