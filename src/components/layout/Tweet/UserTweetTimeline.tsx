838fb71ec2c83d2b607903cbeb1545f8ac110223
mynolog <minholee.log@gmail.com>
2024-12-17 16:08:38 +0900

    🐛 fix: 프로필 페이지에서 tweets 컬렉션이 비어있을 경우 렌더링 UI 버그 수정

--- src/components/layout/Tweet/UserTweetTimeline.tsx
+++ src/components/layout/Tweet/UserTweetTimeline.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import TweetItem from './TweetItem'
 import { Tweet } from '@/types/tweetTypes'
 import { fetchTweetsByUid } from '@/services/tweetServices'
+import { LuBird } from 'react-icons/lu'
 
 export default function UserTweetTimeline({ uid }: { uid: string }) {
   const [tweets, setTweets] = useState<Tweet[]>([])
@@ -21,7 +22,7 @@ export default function UserTweetTimeline({ uid }: { uid: string }) {
 
   return (
     <ul className="w-full mt-12 flex flex-col gap-8">
-      {isLoading || !tweets.length ? (
+      {isLoading ? (
         <div className="relative w-full h-screen flex flex-col gap-9">
           {Array.from({ length: tweets.length }, (_, index) => (
             <div key={index} className="w-5/6 flex gap-4 animate-pulse">
@@ -29,6 +30,11 @@ export default function UserTweetTimeline({ uid }: { uid: string }) {
               <div className="w-11/12 h-28 bg-gray-300 rounded-2xl"></div>
             </div>
           ))}
+        </div>
+      ) : tweets.length === 0 ? (
+        <div className="w-full flex flex-col text-github-gray justify-center items-center">
+          <LuBird className="text-8xl" />
+          <span className="text-center font-bold">첫 번째 트윗을 공유해주세요 :)</span>
         </div>
       ) : (
         tweets.map((tweet) => <TweetItem key={tweet.id} tweet={tweet} />)
